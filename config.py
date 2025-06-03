f3b004af356bca8fb2e620b729c29f6123b601ee
danielalcino <danieldanielalcino@gmail.com>
2025-06-03 20:41:52 +0100

    pipmysql

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ import os
 
 class Config:
     SECRET_KEY = secure_filename(os.urandom(24).hex())
-    SQLALCHEMY_DATABASE_URI = os.getenv('MYSQL_URL')  # ou o nome da variável que você criar no Railway
+    SQLALCHEMY_DATABASE_URI = os.getenv("MYSQL_URL").replace("mysql://", "mysql+pymysql://")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
