cb77eac1a6baf3d456c734c1bcf3686b3c6cb7f6
danielalcino <danieldanielalcino@gmail.com>
2025-06-03 18:48:25 +0100

    sss

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+
 import os
 from werkzeug.utils import secure_filename
 
 class Config:
     SECRET_KEY = secure_filename(os.urandom(24).hex())
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////data/app.db' 
+    # Troque os valores abaixo pelos dados reais do Railway ou Render
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://USUARIO:SENHA@HOST:PORTA/NOME_DO_BANCO'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
