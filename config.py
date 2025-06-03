5a49e74e98c854d558d84b0805cd9a6221869098
danielalcino <danieldanielalcino@gmail.com>
2025-06-03 20:29:36 +0100

    db, db

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 
-import os
 from werkzeug.utils import secure_filename
+import os
 #
+
 class Config:
     SECRET_KEY = secure_filename(os.urandom(24).hex())
-    # Troque os valores abaixo pelos dados reais do Railway ou Render
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cWjRToxNDJyuNosnQkqNRLkKLzqVVLhO@maglev.proxy.rlwy.net:40818/railway'
+    SQLALCHEMY_DATABASE_URI = os.getenv('MYSQL_URL')  # ou o nome da variável que você criar no Railway
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
